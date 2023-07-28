7ed4b70d59837177a0e27dc27f5059ef574e19be
Buzulan Dumitru <bdumitrua@gmail.com>
2023-07-28 20:37:40 +0300

    refactored ListRightsController and did ListRightsService

--- app/Http/Controllers/Api/ApiListRightsController.php
+++ app/Http/Controllers/Api/ApiListRightsController.php
@@ -3,108 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ListRightsRequest;
-use App\Models\ListEditor;
-use App\Models\ListViewer;
 use App\Models\TasksList;
 use App\Models\User;
+use App\Services\ListRightsService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class ApiListRightsController extends Controller
 {
+    private $listRightsService;
+
+    public function __construct(ListRightsService $listRightsService)
+    {
+        $this->listRightsService = $listRightsService;
+    }
+
     public function addEditor(Request $request, TasksList $list)
     {
-        $editor = $this->getClient($request->email);
-        if (!$editor) {
-            return response()->json('User not found', 404);
-        }
-
-        // Проверка, что пользователь является создателем списка
-        $isCreator = $this->isCreator($list);
-        if (!$isCreator) {
-            return response()->json('Unauthorized', 403);
-        }
-
-        if (ListEditor::where('user_id', $editor->id)->where('list_id', $list->id)->count() > 0) {
-            return response()->json('This user is already editor', 422);
-        }
-
-        ListEditor::create([
-            'user_id' => $editor->id,
-            'list_id' => $list->id
-        ]);
-
-        return response()->json('User added as editor', 200);
+        return $this->handleServiceCall(function () use ($request, $list) {
+            return $this->listRightsService->addEditor($request, $list);
+        });
     }
 
     public function removeEditor(TasksList $list, User $editor)
     {
-        // Проверка, что пользователь является создателем списка
-        $isCreator = $this->isCreator($list);
-        if (!$isCreator) {
-            return response()->json('Unauthorized', 403);
-        }
-
-
-        // Удаление пользователя из списка редакторов
-        $list->editors()->detach($editor);
-
-        return response()->json('User removed from editors', 200);
+        return $this->handleServiceCall(function () use ($list, $editor) {
+            return $this->listRightsService->removeEditor($list, $editor);
+        });
     }
 
     public function addViewer(Request $request, TasksList $list)
     {
-        $viewer = $this->getClient($request->email);
-        $user = Auth::user();
-
-        if (!$viewer) {
-            return response()->json('User not found', 404);
-        }
-
-        // Проверка, что пользователь является создателем списка
-        $isCreator = $this->isCreator($list);
-        if (!$isCreator) {
-            return response()->json('Unauthorized', 403);
-        }
-
-
-        if (ListViewer::where('user_id', $viewer->id)->where('list_id', $list->id)->count() > 0) {
-            return response()->json('This user is already viewer', 422);
-        }
-
-        // Добавление пользователя в список просмотрщиков
-        ListViewer::create([
-            'user_id' => $viewer->id,
-            'list_id' => $list->id
-        ]);
-
-        return response()->json('User added as viewer', 200);
+        return $this->handleServiceCall(function () use ($request, $list) {
+            return $this->listRightsService->addViewer($request, $list);
+        });
     }
 
     public function removeViewer(TasksList $list, User $viewer)
     {
-        // Проверка, что пользователь является создателем списка
-        $isCreator = $this->isCreator($list);
-        if (!$isCreator) {
-            return response()->json('Unauthorized', 403);
-        }
-
-        // Удаление пользователя из списка просмотрщиков
-        $list->viewers()->detach($viewer);
-
-        return response()->json('User removed from viewers', 200);
-    }
-
-    private function getClient($email)
-    {
-        $client = User::where('email', $email)->first();
-
-        return $client;
-    }
-
-    private function isCreator($list)
-    {
-        return $list->user_id === Auth::user()->id;
+        return $this->handleServiceCall(function () use ($list, $viewer) {
+            return $this->listRightsService->removeViewer($list, $viewer);
+        });
     }
 }
--- app/Services/ListRightsService.php
+++ app/Services/ListRightsService.php
@@ -2,10 +2,91 @@
 
 namespace App\Services;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ListRightsRequest;
+use App\Models\ListEditor;
+use App\Models\ListViewer;
+use App\Models\TasksList;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ListRightsService
 {
+    public function addEditor(Request $request, TasksList $list)
+    {
+        // Проверка, что пользователь является создателем списка
+        $this->isCreator($list);
+
+        // Поиск пользователя по почте
+        $editor = $this->getClient($request->email);
+
+        if (ListEditor::where('user_id', $editor->id)->where('list_id', $list->id)->count() > 0) {
+            return response()->json('This user is already editor', 422);
+        }
+
+        ListEditor::create([
+            'user_id' => $editor->id,
+            'list_id' => $list->id
+        ]);
+    }
+
+    public function removeEditor(TasksList $list, User $editor)
+    {
+        // Проверка, что пользователь является создателем списка
+        $this->isCreator($list);
+
+        // Удаление пользователя из списка редакторов
+        $list->editors()->detach($editor);
+    }
+
+    public function addViewer(Request $request, TasksList $list)
+    {
+        // Проверка, что пользователь является создателем списка
+        $this->isCreator($list);
+
+        // Поиск пользователя по почте
+        $viewer = $this->getClient($request->email);
+
+        if (ListViewer::where('user_id', $viewer->id)->where('list_id', $list->id)->count() > 0) {
+            return response()->json('This user is already viewer', 422);
+        }
+
+        // Добавление пользователя в список просмотрщиков
+        ListViewer::create([
+            'user_id' => $viewer->id,
+            'list_id' => $list->id
+        ]);
+    }
+
+    public function removeViewer(TasksList $list, User $viewer)
+    {
+        // Проверка, что пользователь является создателем списка
+        $this->isCreator($list);
+
+        // Удаление пользователя из списка просмотрщиков
+        $list->viewers()->detach($viewer);
+    }
+
+    private function getClient($email)
+    {
+        $client = User::where('email', $email)->first();
+
+        if (!$client) {
+            throw new HttpException(Response::HTTP_NOT_FOUND, 'User not found');
+        }
+
+        return $client;
+    }
+
+    private function isCreator($list)
+    {
+        if ($list->user_id !== Auth::user()->id) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Unauthorized');
+        }
+
+        return true;
+    }
 }
